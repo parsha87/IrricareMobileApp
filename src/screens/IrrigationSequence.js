f63929cc97001536f552f09f068f0b518ab74178
Prashant <pashtekar87@gmail.com>
2024-03-07 12:44:31 +0530

    update

--- src/screens/IrrigationSequence.js
+++ src/screens/IrrigationSequence.js
@@ -46,14 +46,14 @@ const IrrigationSequenceScreen = ({ route }) => {
 
     const hideModal = () => setVisible(false);
 
-    const [items, setItems] = useState([
-        { label: 'Sunday', value: '1' },
-        { label: 'Monday', value: '2' },
-        { label: 'Tuesday', value: '3' },
-        { label: 'Wednesday', value: '4' },
-        { label: 'Thrusday', value: '5' },
-        { label: 'Friday', value: '6' },
-        { label: 'Saturday', value: '7' },
+    const [items, setItems] = useState([      
+        { label: 'Monday', value: '1' },
+        { label: 'Tuesday', value: '2' },
+        { label: 'Wednesday', value: '3' },
+        { label: 'Thrusday', value: '4' },
+        { label: 'Friday', value: '5' },
+        { label: 'Saturday', value: '6' },
+        { label: 'Sunday', value: '7' },
     ]);
 
     const showModal = () => setVisible(true);
